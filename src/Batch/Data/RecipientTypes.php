bb4683ee99028f2f211bdfb614476f5683931c1a
Marc-André Appel <marc-andre@hybride-conseil.fr>
2019-01-11 08:37:26 +0100

    Ajouté des classes pour les données à générer

---
+++ src/Batch/Data/RecipientTypes.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * RecipientTypes.php
+ * @author      Marc-André Appel <marc-andre@hybride-conseil.fr>
+ * @copyright   2019 Hybride Conseil
+ * @license     http://opensource.org/licenses/MIT MIT
+ * @link        https://www.hybride-conseil.fr
+ * @created     10/01/2019
+ */
