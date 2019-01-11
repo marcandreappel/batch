1726986b61cccbb84b4a7ce58a7925a143b824d2
Marc-André Appel <marc-andre@hybride-conseil.fr>
2019-01-11 08:38:27 +0100

    Supprimé un fichier redondant

--- src/Batch/Data/RecipientTypes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * RecipientTypes.php
- * @author      Marc-André Appel <marc-andre@hybride-conseil.fr>
- * @copyright   2019 Hybride Conseil
- * @license     http://opensource.org/licenses/MIT MIT
- * @link        https://www.hybride-conseil.fr
- * @created     10/01/2019
- */
